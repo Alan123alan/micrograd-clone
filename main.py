e0dc1027bddb0a4c96b7701b04075b8c80d0394e
Alan123alan <alancastellanos2@hotmail.com>
2023-09-10 23:30:54 -0600

    starting manual backpropagation by derivatives and use of the chain rule from calculus

--- main.py
+++ main.py
@@ -44,10 +44,12 @@ print(f"Slope : {(d2-d1)/h}")
 
 # A class that will help to track trace of operations performed on values
 class Value:
-    def __init__(self, data, _children=(), _op=""):
+    def __init__(self, data, _children=(), _op="", label=""):
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
+        self.label = label
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
@@ -56,11 +58,19 @@ class Value:
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), "*")
         return out
-        
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a * b + c
+
+# Instantiating value objects 
+a = Value(2.0,label="a")
+b = Value(-3.0, label="b")
+c = Value(10.0, label="c")
+d = a * b
+e = d + c
+d.label = "d"
+e.label = "e"
+e.grad = 1.00
+d.grad = 1.00
+c.grad = 1.00
+# Checking the objects are set correctly
 print(f"a data: {a}, children: {a._prev}, op: {a._op}")
 print(f"b data: {b}, children: {b._prev}, op: {b._op}")
 print(f"c data: {c}, children: {c._prev}, op: {c._op}")
@@ -74,20 +84,46 @@ def trace(root):
             nodes.add(node)
             for child in node._prev:
                 edges.add((child, node))
-                print(f"data: {child.data}, children: {child._prev}, op: {child._op}")
                 build(child)
     build(root)
     return nodes, edges
-nodes, edges = trace(d)
+nodes, edges = trace(e)
 print(nodes, edges)
 graph = Digraph(name="Equation relationship", format="png")
 for node in nodes:
-    graph.node(name=str(id(node)), label=f"Data = {node.data}", shape="record") 
+    graph.node(name=str(id(node)), label=f"{node.label} | data = {node.data} | grad = {node.grad}", shape="record") 
     if node._op:
         graph.node(name=f"{id(node)}{node._op}", label=node._op)
         graph.edge(f"{id(node)}{node._op}", str(id(node)))
 for tail, head in edges:
     graph.edge(str(id(tail)), f"{id(head)}{head._op}")
 print(graph.source)
-print(id(graph))
 graph.render(directory=path.dirname(__file__), view=True)
+
+
+def manual_backpropagation():
+    h = 0.00001
+    a = Value(2.0,label="a")
+    b = Value(-3.0, label="b")
+    c = Value(10.0, label="c")
+    d = a * b
+    e1 = d + c
+    d.label = "d"
+    e1.label = "e"
+    d = a * b
+    d.data += h
+    e2 = d + c
+    slope = (e2.data-e1.data)/h
+    print(slope)
+    # The grad of a current node
+    # Is the derivate of previous node with respect
+    # To current node (with exception of the head node)
+    # So if a*b = d and c + d = e
+    # de/de = 1 so this will be the grad for the head node
+    # Then we need to derive de/dd and de/dc to get the grads (slopes)
+    # de/dd with e = d + c -> d(d+c)/dd
+    # By applying the definition of derivative f(x+h)-f(x)/h
+    # We have ((d+h+c)-(d+c))/h -> 1.00
+    # And for de/dc ((d+c+h)-(d+c))/h -> 1.00
+manual_backpropagation()
+
